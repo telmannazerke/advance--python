354d688097a898a54bec47d0f38d6962b23d35de
telmannazerke <241342@astanait.edu.kz>
2025-12-24 18:35:47 +0500

    Delete week-3 directory

--- week-3/task3.2.py
+++
@@ -1,9 +0,0 @@
-s = input()
-
-words = s.split()
-result = []
-
-for word in words:
-    result.append("".join(sorted(word)))
-
-print(" ".join(result))
--- week-3/task7.2.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Enter a non-negative integer: "))
-
-octal = oct(n)[2:]
-
-octal = octal.zfill(10)
-
-print(octal)
